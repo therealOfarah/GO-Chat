c4435ff1afefaa92d6bb59eb3775fe1327487361
Omar Farah <omaarfaarah2019@gmail.com>
2023-01-01 11:25:15 -0500

    add websocket server

--- back-end/main.go
+++ back-end/main.go
@@ -1,7 +1,48 @@
 package main
-import(
+
+import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+var upgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+	// this will to make requests from our React development server to here.
+	CheckOrigin:func(r *http.Request) bool {return true},
+}
+func reader(conn *websocket.Conn){
+	for{
+		messageType,p,err:= conn.ReadMessage()
+		if err !=nil{
+			log.Print(err)
+			return
+		}
+		fmt.Println(string(p))
+		if err := conn.WriteMessage(messageType, p); err!=nil{
+			log.Println(err)
+			return
+		}
+	}
+}
+func serverWs(w http.ResponseWriter, r * http.Request){
+	fmt.Println(r.Host)
+
+	ws, err := upgrader.Upgrade(w,r,nil)
+	if err != nil{
+		log.Println(err)
+	}
+	reader(ws)
+}
+func setupRoutes(){
+	http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request) {
+		fmt.Print(w,"Simple server")
+	})
+	http.HandleFunc("/ws",serverWs)
+}
 func main(){
-	fmt.Print("omar")
+	setupRoutes()
+	http.ListenAndServe(":8080",nil)
 }
